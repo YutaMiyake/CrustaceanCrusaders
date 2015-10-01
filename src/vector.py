d1d0a61389392d4b2d33b2b1c24e2c1471c6ef0f
YutaMiyake <ymiyake@nevada.unr.edu>
2015-09-30 22:50:02 -0700

    modified

--- src/vector.py
+++ src/vector.py
@@ -1,9 +1,2 @@
-# Feb 20, Decided to use ogre's vector class. Overloads +,-, ... correctly
-# and we don't neet yet another vector math library
-#
-# Vector module for 38engine
-# Sushil Louis
-#
-
 import ogre.renderer.OGRE as ogre
 MyVector  = ogre.Vector3
